9c13d9ea6bd2cba6e572dfb4af0c7cedd43d8aba
jahanzeb-Ali <jahanzebsoomro543@gmail.com>
2024-05-05 23:35:56 +0500

    Task # 7

---
+++ app.js
@@ -0,0 +1,11 @@
+// 7. Write a JavaScript program to check whether a given positive number is a 
+// multiple of 3 or 7. 
+
+// let user_value = +prompt("Please Enter positive Number: ")
+// if(user_value% 2==0){
+//     if(user_value%3==0||user_value%7==0){
+//         console.log("Yes")
+//     }
+// }else{
+//     console.log("Not a positive number")
+// }
